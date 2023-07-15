2b1e5103669d5166bb5ab8a08aeb1118af99ef73
Mykola Bezpalko <m.bezpalko@yahoo.com>
2023-07-15 15:41:00 +0300

    Refactoring: 1) updated README file 2) Renamed class 'Logic' to PriceUtil 3) Splitted getHistory() to new methods 4) updated variables naming for better understanding 5) Maven has been added to the project

--- src/PriceUtil.java
+++ src/PriceUtil.java
@@ -1,5 +1,7 @@
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -15,16 +17,11 @@ public class Logic {
     public static int dayCounter = 0;
     static URL infoCurrentPrice;
     static URL infoHistory;
-    static final int dayMilliseconds = 86400000;
-    static Map<String, Double> historyMap = new LinkedHashMap<>();
+    static Map<String, Double> history = new LinkedHashMap<>();
 
     static {
         try {
             infoCurrentPrice = new URL("https://api.coindesk.com/v1/bpi/currentprice/USD.json");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
             infoHistory = new URL("https://api.coindesk.com/v1/bpi/historical/close.json");
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -32,12 +29,9 @@ public class Logic {
     }
 
     static JSONObject getCurrentPriceInfo() throws Exception {
-
         try (InputStream in = infoCurrentPrice.openStream()) {
-            Files.copy(in, Paths.get("resources\\bitcoinCurrentPrice.json"),
-                    StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(in, Paths.get("resources\\bitcoinCurrentPrice.json"), StandardCopyOption.REPLACE_EXISTING);
         }
-
         JSONParser parser = new JSONParser();
         FileReader reader = new FileReader("resources\\bitcoinCurrentPrice.json");
         Object obj = parser.parse(reader);
@@ -50,12 +44,10 @@ public class Logic {
         return (String) time.get("updated");
     }
 
-    static JSONObject getPricesHistory() throws Exception {
+    static JSONObject getPricesHistoryFromJSON() throws IOException, ParseException {
         try (InputStream in = infoHistory.openStream()) {
-            Files.copy(in, Paths.get("resources\\bitcoinHistoryPrice.json"),
-                    StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(in, Paths.get("resources\\bitcoinHistoryPrice.json"), StandardCopyOption.REPLACE_EXISTING);
         }
-
         JSONParser parser = new JSONParser();
         FileReader reader = new FileReader("resources\\bitcoinHistoryPrice.json");
         Object obj = parser.parse(reader);
@@ -63,9 +55,9 @@ public class Logic {
         return (JSONObject) obj;
     }
 
-    static void getHistory() throws Exception {
-        JSONObject bpiHistory = (JSONObject) Logic.getPricesHistory().get("bpi");
-
+    static void getHistory() throws IOException, ParseException {
+        int milliSecondOfDay = 86400000;
+        JSONObject bitCoinHistory = (JSONObject) Logic.getPricesHistoryFromJSON().get("bpi");
         String pattern = "yyyy-MM-dd";
         SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.getDefault());
 
@@ -73,13 +65,13 @@ public class Logic {
         String sDate;
         Calendar calendar = new GregorianCalendar();
         while(true){
-            calendar.setTimeInMillis(calendar.getTimeInMillis() - dayMilliseconds);
+            calendar.setTimeInMillis(calendar.getTimeInMillis() - milliSecondOfDay);
             bufferDate = calendar.getTime();
             sDate = formatter.format(bufferDate);
-            Double prices = (Double) bpiHistory.get(sDate);
+            Double prices = (Double) bitCoinHistory.get(sDate);
             if(prices != null){
                 dayCounter++;
-                historyMap.put(sDate, prices);
+                history.put(sDate, prices);
             }
             if(prices == null){
                 return;
@@ -87,4 +79,5 @@ public class Logic {
         }
 
     }
+    
 }
